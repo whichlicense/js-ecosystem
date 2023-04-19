e8e6b0cf9285630888fc033308174fb533069332
David Greven <opensource@grevend.dev>
2023-04-19 18:29:46 +0200

    Create NpmPackageSeeker & NpmPackageLockSeeker for js-ecosystem@npm
    Signed-off-by: David Greven <opensource@grevend.dev>

---
+++ npm/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/whichlicense/js-ecosystem.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import com.whichlicense.metadata.seeker.MetadataSeeker;
+import com.whichlicense.metadata.seeker.npm.NpmPackageLockSeeker;
+import com.whichlicense.metadata.seeker.npm.NpmPackageSeeker;
+
+module whichlicense.seeker.npm {
+    requires transitive whichlicense.seeker;
+    exports com.whichlicense.metadata.seeker.npm;
+    provides MetadataSeeker with NpmPackageSeeker, NpmPackageLockSeeker;
+}
