de19e40d26373fd11f384aaca93f524b898d0968
David Greven <opensource@grevend.dev>
2023-04-20 12:00:16 +0200

    Create YarnLockSeeker for js-ecosystem@yarn

---
+++ yarn/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/whichlicense/js-ecosystem.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import com.whichlicense.metadata.seeker.MetadataSeeker;
+import com.whichlicense.metadata.seeker.yarn.YarnLockSeeker;
+
+module whichlicense.seeker.yarn {
+    requires transitive whichlicense.seeker;
+    exports com.whichlicense.metadata.seeker.yarn;
+    provides MetadataSeeker with YarnLockSeeker;
+}
